e0665a1f70fbe10b57b552f294c82585dda61c71
PabloRC0525 <121038827@upq.edu.mx>
2024-03-12 15:14:10 -0600

    Se corrigieron vistas y se modificó la navbar

--- resources/views/cliente_solicitar.blade.php
+++ resources/views/cliente_solicitar.blade.php
@@ -2,30 +2,17 @@
 @section('contenido')
 <body class="bg-gray-100">
     <div class="container mx-auto p-6">
-        <div class="bg-white shadow-md p-6 rounded">
+        <div class="bg-gray-100 shadow-md p-6 rounded">
+            <h2 class="text-2xl font-bold m-4">Titulo</h2>
+            <input type="text" name="Descripcion" id="desc" autocomplete="desc" required class="mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md h-7">
             <h2 class="text-2xl font-bold mb-4">Descripcion</h2>
-            <p class="text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-        </div>
-        <div class="bg-white shadow-md p-6 mt-6 rounded">
-            <h2 class="text-2xl font-bold mb-4">Enviar Solicitud</h2>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <input type="text" name="Descripcion" id="desc" autocomplete="desc" required class="mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md h-20">
+            <h2 class="text-2xl font-bold m-4">Enviar Solicitud</h2>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
                 Enviar
             </button>
-        </div>
-        <div class="bg-white shadow-md p-6 mt-6 rounded">
-            <h2 class="text-2xl font-bold mb-4">Titulo</h2>
-            <p class="text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-        </div>
-        <div class="bg-white shadow-md p-6 mt-6 rounded flex justify-end">
-            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2">
+            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m 4">
                 Cancelar
-            </button>
-            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                Descripcion
             </button>
         </div>
     </div>
